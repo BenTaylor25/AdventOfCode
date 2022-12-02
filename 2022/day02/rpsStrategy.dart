dcded4be12d6e5004b95c1a2d0c5c07d1e14efad
benlaptop25 <ben.taylor2573@gmail.com>
2022-12-02 12:19:56 +0000

    fixed order

--- 2022/day02/rpsStrategy.dart
+++ 2022/day02/rpsStrategy.dart
@@ -2,7 +2,7 @@
 import 'dart:io';
 
 /**
- * returns [playerRps, elfRps]
+ * returns [elfRps, playerRps]
  * 
  * 0 = rock,
  * 1 = paper,
@@ -12,10 +12,10 @@ List getGameDecision(List lineAsList) {
   assert('ABC'.contains(lineAsList[0]));
   assert('XYZ'.contains(lineAsList[1]));
 
-  int playerAscii = lineAsList[0].codeUnitAt(0);
-  int elfAscii = lineAsList[1].codeUnitAt(0);
+  int elfAscii = lineAsList[0].codeUnitAt(0);
+  int playerAscii = lineAsList[1].codeUnitAt(0);
 
-  return [playerAscii - 65, elfAscii - 88];
+  return [elfAscii - 65, playerAscii - 88];
 }
 
 /**
@@ -25,14 +25,14 @@ List winLose(List game) {
   assert([0, 1, 2].contains(game[0]));
   assert([0, 1, 2].contains(game[1]));
 
-  bool playerWins = false;
+  bool playerLoses = false;
   if (game[0] - 1 == game[1] || game[0] + 2 == game[1]) {
-    playerWins = true;
+    playerLoses = true;
   }
 
-  bool playerLoses = false;
+  bool playerWins = false;
   if (game[0] + 1 == game[1] || game[0] - 2 == game[1]) {
-    playerLoses = true;
+    playerWins = true;
   }
 
   return [playerWins, playerLoses];
