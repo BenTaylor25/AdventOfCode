32c7ffeb1aaccb1094d1c6dcdf140470a1038f52
benlaptop25 <ben.taylor2573@gmail.com>
2022-12-04 12:53:16 +0000

    get_contains_count()

--- 2022/day04/count_contains/src/main.rs
+++ 2022/day04/count_contains/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-fn read_pair() -> Vec<[i32; 4]> {
+fn read_pairs() -> Vec<[i32; 4]> {
     // run from count_contains dir
     let contents = fs::read_to_string("./assignmentSample.txt")
         .expect("Cannot read file.");
@@ -38,6 +38,29 @@ fn read_pair() -> Vec<[i32; 4]> {
     return pairs_as_vec
 }
 
+fn contains(pair: [i32; 4]) -> bool {
+    if pair[0] == pair[2] {
+        return true;
+    }
+
+    let diff_product = (pair[0] - pair[2]) * (pair[1] - pair[3]);
+
+    return diff_product <= 0;
+}
+
+fn get_contains_count(pairs: Vec<[i32; 4]>) -> i32 {
+    let mut count = 0;
+    for pair in pairs {
+        if contains(pair) {
+            count += 1;
+        }
+    }
+    return count;
+}
+
 fn main() {
-    read_pair();
+    let pairs = read_pairs();
+    let contains_count = get_contains_count(pairs);
+
+    println!("{}", contains_count);
 }
