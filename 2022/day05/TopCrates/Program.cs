9564210c1eb7e2ecf7dea45c0890d7adc2697171
benlaptop25 <ben.taylor2573@gmail.com>
2022-12-05 10:05:30 +0000

    added colCount comment

--- 2022/day05/TopCrates/Program.cs
+++ 2022/day05/TopCrates/Program.cs
@@ -15,6 +15,16 @@ class Program {
     private static List<char> ReadCrateState(string filename) {
 
         string[] crateLines = ReadCrateLines(filename);
+
+        // columns are represented with 3 characters, joined by a space
+        // `123 123 123`
+        // note that there is no whitespace following the last character
+        //
+        // let c = colCount
+        // let l = lineLength
+        // l = 3c + (c-1)
+        // l = 4c - 1
+        // c = (l + 1) / 4
         int colCount = (crateLines[0].Length + 1) / 4;
 
         Console.WriteLine(colCount);
