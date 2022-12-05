455dcbcef9306af7a0eab6b75da7b98fe47d8e62
benlaptop25 <ben.taylor2573@gmail.com>
2022-12-05 18:14:06 +0000

    switched to actual file

--- 2022/day05/MultipleCrates/Program.cs
+++ 2022/day05/MultipleCrates/Program.cs
@@ -95,7 +95,7 @@ class Program {
     }
 
     public static void Main() {
-        string filename = "./cratesSample.txt";
+        string filename = "./cratesActual.txt";
 
         List<List<char>> crates = GetCrateState(filename);
 
