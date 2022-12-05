1cbd5e6d72d716e861374daf1e16ad92b96113bc
benlaptop25 <ben.taylor2573@gmail.com>
2022-12-05 09:49:57 +0000

    split input file

--- 2022/day05/TopCrates/Program.cs
+++ 2022/day05/TopCrates/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class Program {
+    private static List<char> ReadCrateState(string filename) {
+
+        string[] lines = System.IO.File.ReadAllLines(filename);
+        int delim = -1;
+        for (int i = 0; i < lines.Length; i++) {
+            if (lines[i] == "") {
+                delim = i;
+            } 
+        }
+
+        for (int i = 0; i < delim; i++) {
+            Console.WriteLine(lines[i]);
+        }
+
+        return new List<char>();
+    }
+
+    public static void Main() {
+        string filename = "./cratesSample.txt";
+
+        List<char> crates = ReadCrateState(filename);
+    }
+}
+
