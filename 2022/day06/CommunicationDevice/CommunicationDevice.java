064fe5120a2d0d549b50a8578cb69a4eb9fc09e4
benlaptop25 <ben.taylor2573@gmail.com>
2022-12-06 09:51:56 +0000

    part 1 scope

---
+++ 2022/day06/CommunicationDevice/CommunicationDevice.java
@@ -0,0 +1,11 @@
+
+public class CommunicationDevice {
+
+    public static void main(String[] args) {
+        // given a string
+        // find the first group of 4 unique characters
+        // return the index of the 4th (1-indexed)
+
+        String filename = "commSample";
+    }
+}
