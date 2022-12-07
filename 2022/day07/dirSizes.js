3976f6d553b9d8f0c12e74378573f2a1899efcf8
benlaptop25 <ben.taylor2573@gmail.com>
2022-12-07 12:02:21 +0000

    parse text file

--- 2022/day07/dirSizes.js
+++ 2022/day07/dirSizes.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 
-function readDirs(filename) {
+function readCommands(filename) {
     try {
         let data = fs.readFileSync(filename, 'utf8');
         return data.split('\n');
@@ -11,12 +11,65 @@ function readDirs(filename) {
     }
 }
 
+function getCurrentObjLocation(base, chain) {
+    let current = base;
+
+    chain.forEach(key => {
+        current = current[key];
+    });
+
+    return current;
+}
+
+function makeObj(lines) {
+    let base = {'/': {}};
+    let chain = [];
+
+    let listingDirs = false;
+
+    lines.forEach(line => {
+        let lineArr = line.split(' ');
+        let isCommand = lineArr[0] == '$';
+
+        if (isCommand) { 
+            listingDirs = false; 
+        } else if (listingDirs) {
+            let current = getCurrentObjLocation(base, chain);
+            if (lineArr[0] == 'dir') {
+                current[lineArr[1]] = {};
+            } else {
+                current[lineArr[1]] = lineArr[0]
+            }
+        }
+
+        if (isCommand && lineArr[1] == 'cd') {
+            if (lineArr[2] == '..') {
+                chain.pop();
+            } else {
+                chain.push(lineArr[2]);
+            }
+        }
+
+        else if (isCommand && lineArr[1] == 'ls') {
+            listingDirs = true;
+        }
+        
+    });
+
+
+    return base;
+}
+
+function readDirsAsArr(filename) {
+    let lines = readCommands(filename);
+    let obj = makeObj(lines);
+
+    console.log(obj['/']);
+
+    return; //
+}
 
 function main() {
-    let lines = readDirs('dirSample.txt');
-
-    if (lines) {
-        console.log(lines);
-    }
+    let dirs = readDirsAsArr('dirSample.txt');
 }
 main();
