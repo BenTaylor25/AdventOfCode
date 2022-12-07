3d4c5a113ba09e177c96439965069d34031d1c60
benlaptop25 <ben.taylor2573@gmail.com>
2022-12-07 14:19:54 +0000

    min dir to delete to free enough space for update

---
+++ 2022/day07/dirToDelete.js
@@ -0,0 +1,139 @@
+const fs = require('fs');
+
+
+function readCommands(filename) {
+    try {
+        let data = fs.readFileSync(filename, 'utf8');
+        return data.split('\n');
+    } catch (e) {
+        console.error(e);
+        return []
+    }
+}
+
+function getCurrentObjLocation(base, chain) {
+    let current = base;
+
+    chain.forEach(key => {
+        current = current[key];
+    });
+
+    return current;
+}
+
+function makeObj(lines) {
+    let base = {'/': {}};
+    let chain = [];
+
+    let listingDirs = false;
+
+    lines.forEach(line => {
+        let lineArr = line.split(' ');
+        let isCommand = lineArr[0] == '$';
+
+        if (isCommand) { 
+            listingDirs = false; 
+        } else if (listingDirs) {
+            let current = getCurrentObjLocation(base, chain);
+            if (lineArr[0] == 'dir') {
+                current[lineArr[1]] = {};
+            } else {
+                current[lineArr[1]] = --lineArr[0] + 1
+            }
+        }
+
+        if (isCommand && lineArr[1] == 'cd') {
+            if (lineArr[2] == '..') {
+                chain.pop();
+            } else {
+                chain.push(lineArr[2]);
+            }
+        }
+
+        else if (isCommand && lineArr[1] == 'ls') {
+            listingDirs = true;
+        }
+        
+    });
+
+
+    return base;
+}
+
+function objToArr(obj) {
+    if (typeof obj == 'object') {
+        let arr = [];
+        for (const [_, v] of Object.entries(obj)) {
+            arr.push(objToArr(v));
+        }
+        return arr;
+    }
+    return obj;
+}
+
+function readDirsAsArr(filename) {
+    let lines = readCommands(filename);
+    let obj = makeObj(lines);
+    let arr = objToArr(obj)[0];
+
+    // console.log(obj['/']);
+    // console.log();
+    // console.log(arr);
+
+    return arr;
+}
+
+function recSum(arr) {
+    sum = 0;
+
+    arr.forEach(num => {
+        if (typeof num == 'object') {   // Array
+            sum += recSum(num);
+        } else {
+            sum += num;
+        }
+    });
+
+    return sum;
+}
+
+function getAllSums(dirsArr) {
+    let allSums = [];
+    allSums.push(recSum(dirsArr))
+
+    dirsArr.forEach(item => {
+        if (typeof item == 'object') {   // Array
+            let recSums = getAllSums(item);
+            allSums.push(...recSums);
+        }
+    });
+
+    return allSums;
+}
+
+function min(arr) {
+    let m = arr[0];
+
+    arr.forEach(item => {
+        if (item < m) {
+            m = item;
+        } 
+    });
+
+    return m;
+}
+
+function main() {
+    let dirsArr = readDirsAsArr('dirSample.txt');
+    let allSums = getAllSums(dirsArr);
+
+    let wholeFs = allSums[0];
+    let remainingRequired = 30_000_000 - (70_000_000 - wholeFs);
+
+    let filteredSums = allSums.filter(x => { return x >= remainingRequired });
+    let minFiltered = min(filteredSums);
+
+    console.log(minFiltered);
+}
+main();
+
