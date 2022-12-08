be54a0028333062ca24fee145f43dbfe22082c12
benlaptop25 <ben.taylor2573@gmail.com>
2022-12-08 09:54:50 +0000

    wip count hidden

--- 2022/day08/count_visible.c
+++ 2022/day08/count_visible.c
@@ -1,6 +1,7 @@
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define SIZE 5
 
@@ -33,12 +34,51 @@ int main() {
     int * trees = malloc(SIZE * SIZE * sizeof(int));
     read_ints("./treesSample.txt", &trees);
 
+    // display trees
+    // for (int i = 0; i < SIZE; i++) {
+    //     for (int j = 0; j < SIZE; j++) {
+    //         printf("%d ", trees[i*SIZE + j]);
+    //     }
+    //     printf("\n");
+    // }
+
+    int visible = 0;
+
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
-            printf("%d ", trees[i*SIZE + j]);
+            bool on_perim = i == 0 || j == 0 || i == SIZE-1 || j == SIZE-1;
+
+            if (on_perim) {
+                visible++;
+            } else {
+                bool left_hidden = false;
+                for (int k = 0; k < j; k++) {
+                    left_hidden = left_hidden || trees[i*SIZE + k] > trees[i*SIZE + j];
+                }
+
+                bool right_hidden = false;
+                for (int k = j+1; k < SIZE; k++) {
+                    right_hidden = right_hidden || trees[i*SIZE + k] > trees[i*SIZE + j];
+                }
+
+                bool up_hidden = false;
+                for (int k = 0; k < i; k++) {
+                    up_hidden = up_hidden || trees[k*SIZE + j] > trees[i*SIZE + j];
+                }
+
+                bool down_hidden = false;
+                for (int k = i+1; k < SIZE; k++) {
+                    down_hidden = down_hidden || trees[k*SIZE + j] > trees[k*SIZE + j];
+                }
+
+                if (!(left_hidden && right_hidden && up_hidden && down_hidden)) {
+                    visible++;
+                }
+            }
         }
-        printf("\n");
     }
+
+    printf("%d\n", visible);
 
     free(trees);
 }
