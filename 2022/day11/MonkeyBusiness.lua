c1bb40a7cf0d0290a0f5551eebdb07f4b4785d81
benlaptop25 <ben.taylor2573@gmail.com>
2022-12-11 13:09:30 +0000

    monkey business

--- 2022/day11/MonkeyBusiness.lua
+++ 2022/day11/MonkeyBusiness.lua
@@ -29,6 +29,7 @@ end
 local file = 'monkeysSample.txt'
 local lines = lines_from(file)
 local monkeyArrStruct = {}
+local allInds = {}
 local currentInd = -1
 
 for k,v in pairs(lines) do
@@ -39,10 +40,12 @@ for k,v in pairs(lines) do
         print()
 
     elseif linesplit[1] == "Monkey" then
+        table.insert(allInds, linesplit[2])
         currentInd = linesplit[2]
         monkeyArrStruct[currentInd] = {}
         monkeyArrStruct[currentInd]["items"] = {}
         monkeyArrStruct[currentInd]["truefalse"] = {}
+        monkeyArrStruct[currentInd]["inspected"] = 0
 
     elseif linesplit[1] == "Starting" then
         for i, v in pairs(linesplit) do
@@ -74,6 +77,61 @@ for k,v in pairs(lines) do
 
 end
 
+-- print(monkeyArrStruct[currentInd]["tst"](monkeyArrStruct[currentInd]["op"](14)))
 
-print(monkeyArrStruct[currentInd]["tst"](monkeyArrStruct[currentInd]["op"](14)))
+function MonkeyAction(ind)
+    local items = monkeyArrStruct[ind]["items"]
+    while tablelen(items) > 0 do
+        local item = table.remove(items, 1)
 
+        -- inspect
+        item = monkeyArrStruct[ind]["op"](item)
+        monkeyArrStruct[ind]["inspected"] = monkeyArrStruct[ind]["inspected"] + 1
+
+        -- bored
+        item = item // 3
+
+        -- check divisible --
+        local b = monkeyArrStruct[ind]["tst"](item)
+
+        local passto = ""
+        if b then
+            passto = monkeyArrStruct[ind]["truefalse"][1] .. ":"
+        else
+            passto = monkeyArrStruct[ind]["truefalse"][2] .. ":"
+        end
+
+        -- pass to
+        table.insert(monkeyArrStruct[passto]["items"], item)
+    end
+end
+
+for round = 1, 20 do
+    for _, ind in pairs(allInds) do
+        MonkeyAction(ind)
+    end
+end
+
+print()
+
+local inspected = {}
+for _, ind in pairs(allInds) do
+    table.insert(inspected, monkeyArrStruct[ind]["inspected"])
+end
+
+function popMax(t)
+    local maxi = 1
+    for i, v in pairs(t) do
+        if v > t[maxi] then
+            maxi = i
+        end
+    end
+    return table.remove(t, maxi)
+end
+
+local m1 = popMax(inspected)
+local m2 = popMax(inspected)
+
+local monkeyBusiness = m1 * m2
+
+print(monkeyBusiness)
