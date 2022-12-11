46420ce41379644613841c9241de8cd4150bea5e
benlaptop25 <ben.taylor2573@gmail.com>
2022-12-11 11:53:57 +0000

    all commands

--- 2022/day11/MonkeyBusiness.lua
+++ 2022/day11/MonkeyBusiness.lua
@@ -1,14 +1,11 @@
 -- http://lua-users.org/wiki/FileInputOutput
 
--- see if the file exists
 function file_exists(file)
   local f = io.open(file, "rb")
   if f then f:close() end
   return f ~= nil
 end
 
--- get all lines from a file, returns an empty 
--- list/table if the file does not exist
 function lines_from(file)
   if not file_exists(file) then return {} end
   local lines = {}
@@ -24,21 +21,61 @@ function tablelen(t)
     return count
 end
 
--- tests the functions above
+function intof(s)
+    local nocomma = string.gsub(s, "%,", "")
+    return tonumber(nocomma)
+end
+
 local file = 'monkeysSample.txt'
 local lines = lines_from(file)
+local monkeyArrStruct = {}
+local currentInd = -1
 
--- print all line numbers and their contents
 for k,v in pairs(lines) do
     local linesplit = {}
     for word in v:gmatch("%S+") do table.insert(linesplit, word) end
 
     if tablelen(linesplit) == 0 then
         print()
+
+    elseif linesplit[1] == "Monkey" then
+        currentInd = linesplit[2]
+        monkeyArrStruct[currentInd] = {}
+        monkeyArrStruct[currentInd]["items"] = {}
+
+    elseif linesplit[1] == "Starting" then
+        for i, v in pairs(linesplit) do
+            if i > 2 then
+                table.insert(monkeyArrStruct[currentInd]["items"], intof(v))
+            end
+        end
+
+    elseif linesplit[1] == "Operation:" then
+        monkeyArrStruct[currentInd]["op"] = load(string.format([[
+            local old = ...
+            return old %s %s
+        ]], linesplit[5], linesplit[6]))
+
+    elseif linesplit[1] == "Test:" then
+        monkeyArrStruct[currentInd]["tst"] = load(string.format([[
+            local op = ...
+            return op %% %s == 0
+        ]], linesplit[4]))
+    
+    elseif linesplit[1] == "If" then
+        return linesplit[6]
+
     end
 
     for k2, w in pairs(linesplit) do
         print(w)
     end
     print("-")
+
 end
+
+-- why aren't these 3 lines printing??
+print("?")
+print(monkeyArrStruct[currentInd]["tst"](monkeyArrStruct[currentInd]["op"](14)))
+print("?")
+
