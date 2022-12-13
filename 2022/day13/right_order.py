53823b48081a1cf7755db6be3624795d49f651b9
benlaptop25 <ben.taylor2573@gmail.com>
2022-12-13 18:44:19 +0000

    righ order count

--- 2022/day13/right_order.py
+++ 2022/day13/right_order.py
@@ -1,3 +1,4 @@
+LIST_TYPE = type(list())
 
 def get_lines(filename):
     lines = []
@@ -22,10 +23,51 @@ def get_pairs(filename):
 
     return all_pairs
 
+def compare_lists(a, b):
+    assert type(a) == LIST_TYPE
+    assert type(b) == LIST_TYPE
+    zab = list(zip(a, b))
+
+    i = -1
+    comp_value = 0
+    while comp_value == 0 and i < min(len(a), len(b))-1:
+        i += 1
+        ai, bi = zab[i]
+
+        if type(ai) == type(bi) == LIST_TYPE:
+            comp_value = compare_lists(ai, bi)
+        elif type(ai) == LIST_TYPE:
+            comp_value = compare_lists(ai, [bi])
+        elif type(bi) == LIST_TYPE:
+            comp_value = compare_lists([ai], bi)
+        else:
+            # both int
+            if ai < bi:
+                comp_value = 1
+            elif ai > bi:
+                comp_value = -1
+
+    if comp_value == 0:
+        if i == len(a) - 1:
+            comp_value = 1
+        else:
+            comp_value = -1
+
+    return comp_value
+
+
 def right_order():
     pairs = get_pairs("packetsSample.txt")
-    for p in pairs:
-        print(p)
+    right_pairs = []
+
+    current_pair = 0
+    for pair in pairs:
+        current_pair += 1
+        if compare_lists(pair[0], pair[1]) == 1:
+            right_pairs.append(current_pair)
+    
+    print(sum(right_pairs))
+
 
 if __name__ == '__main__':
     right_order()
