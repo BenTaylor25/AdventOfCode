0912296da43bb1bb94e03ac9f3b2a4d36debd9b5
benlaptop25 <ben.taylor2573@gmail.com>
2022-12-14 16:35:27 +0000

    populate grid

--- 2022/day14/SandFall/sandFall.go
+++ 2022/day14/SandFall/sandFall.go
@@ -5,6 +5,8 @@ import (
 	"bufio"
 	"os"
 	"log"
+	"strings"
+	"strconv"
 )
 
 func getLines(filename string) []string {
@@ -40,12 +42,49 @@ func createEmptyGrid(displaySize int) [][]string {
 	return grid
 }
 
+func populateGrid(grid [][]string, lines []string, displaySize int) [][]string {
+	leftX := 500 - displaySize
+
+
+	for _, line := range lines {
+		linesplit := strings.Split(line, " -> ")
+		
+		for i := 0; i < len(linesplit)-1; i++ {
+			coord1s := strings.Split(linesplit[i], ",")
+			coord2s := strings.Split(linesplit[i+1], ",")
+
+			fromX, _ := strconv.Atoi(coord1s[0])
+			toX, _ := strconv.Atoi(coord2s[0])
+			if fromX > toX {
+				fromX, toX = toX, fromX
+			}
+			fromY, _ := strconv.Atoi(coord1s[1])
+			toY, _ := strconv.Atoi(coord2s[1])
+			if fromY > toY {
+				fromY, toY = toY, fromY
+			}
+			
+			for c := fromX; c <= toX; c++ {
+				for r := fromY; r <= toY; r++ {
+					grid[r][c-leftX] = "#"
+				}
+			}
+
+		}
+		fmt.Println()
+	}
+
+	return grid
+}
+
 func main() {
-	// lines := getLines("rockSample.txt")
+	lines := getLines("rockSample.txt")
 	displaySize := 7   // how far left/right from centre
 
 	grid := createEmptyGrid(displaySize)
+	grid = populateGrid(grid, lines, displaySize)
 
+	fmt.Println()
 	for _, row := range grid {
 		for _, object := range row {
 			fmt.Print(object)
