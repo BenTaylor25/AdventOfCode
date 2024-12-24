79d5d6f277a0a8a42b159bd946ddb641218f033e
benlaptop25 <ben.taylor2573@gmail.com>
2024-12-24 21:08:24 +0000

    2024 day 2 part 1

---
+++ 2024/day02/python/reactor_safety_check.py
@@ -0,0 +1,52 @@
+
+FILENAME = "reactor_levels.txt"
+SAMPLE_FILENAME = "sample_reactor_levels.txt"
+
+MIN = 1
+MAX = 3
+
+def get_reports_from_file(filename):
+    reports = []
+    with open(filename) as f:
+        for line in f:
+            report_as_str = line.strip('\n').split(' ')
+            report_as_int = [int(x) for x in report_as_str]
+            reports.append(report_as_int)
+
+    return reports
+
+def is_report_safe(report, min_delta, max_delta):
+    if len(report) < 2:
+        # Edge case?
+        return True
+
+    if report[0] == report[1]:
+        return False
+
+    should_descend = report[0] > report[1]
+
+    for i in range(len(report) - 1):
+        current_level = report[i]
+        next_level = report[i + 1]
+
+        is_descending = current_level > next_level
+
+        if should_descend != is_descending:
+            return False
+
+        if not min_delta <= abs(current_level - next_level) <= max_delta:
+            return  False
+
+    return True
+
+
+def reactor_safety_check(filename, min_delta, max_delta):
+    reports = get_reports_from_file(filename)
+
+    reports_safety_validation = list(map(lambda x : is_report_safe(x, min_delta, max_delta), reports))
+
+    number_of_safe_reports = reports_safety_validation.count(True)
+    return number_of_safe_reports
+
+if __name__ == "__main__":
+    print(reactor_safety_check(FILENAME, MIN, MAX))
